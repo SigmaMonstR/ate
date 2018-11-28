55db29cc9648a02bf81c3a1d3d4557f3b3c3c267
sigmamonstr <jchen85@gmail.com>
2018-11-27 21:59:10 -0500

    prototype v1

--- script/prototype.R
+++ script/prototype.R
@@ -5,22 +5,29 @@
 n <- 10000
 df <- data.frame(id = 1:n,
                  time = round((1:n)/100),
-                 y = 1:n + rnorm(n,30,300),
+                 y = as.numeric(1:n + rnorm(n,30,300)),
                  x1 = rnorm(n, 100,100),
                  x2 = rnorm(n, 200,100),
-                 x3 = runif(n)*(1:n))
-tt <- runif(nrow(df)) > 0.6
+                 x3 = runif(n)*(1:n),
+                 x4 = rnorm(n, 100,100),
+                 x5 = rnorm(n, 200,100),
+                 x6 = runif(n)*(1:n))
+df <- df[complete.cases(df),]
+tt <- runif(nrow(df)) >= 0.6
 
 
 args <- list(formula = as.formula("y~x1+x2+x3"),
              data = df,
              treatment = tt,
              window = 2,
-             w.size = 30,
+             w.size = 10,
+             w.factor = 5,
              algo = 2,
              run.var = "time",
+             rdd.thresh = 50,
              kval = 10,
              save = FALSE)
+
 autoTrain <- function(args){
   
 #PART 1: Triage components
@@ -29,6 +36,10 @@ autoTrain <- function(args){
   #Check if formula exists
     if(class(args$formula) == "formula"){
       form.obj <- args$formula
+      yvar.name <- as.character(form.obj)[2]
+      xvar.name <- unlist(strsplit(as.character(form.obj)[3], split = "\\+"))
+      xvar.name <- trimws(xvar.name)
+      
       message("..Formula detected")
     } else {
       warning("Missing formula argument.")
@@ -43,16 +54,16 @@ autoTrain <- function(args){
       warning("Missing data frame")
       break
     }
-
-  #Check if data exists
-    if(class(args$data) == "data.frame"){
-      data <- args$data
-      message("..Data detected")
-    } else {
-      warning("Missing data frame")
-      break
-    }
   
+  #Check if there are NAs
+    for(i in c(yvar.name,xvar.name)){
+      if(sum(is.na(data[,i])) > 0){
+        warning(paste0("Missing values detected in variable ", i))
+        break
+      }
+    }
+
+
   #Check if Treatment boolean exists
     if(class(args$treatment) == "logical"){
       treatment <- args$treatment
@@ -66,10 +77,11 @@ autoTrain <- function(args){
   #Check if kfolds var is specified
     if(class(args$kval) == "numeric"){
       kval <- args$kval
-      train_folds <- rep(1:kval, round(nrow(data)/kval)+1)
-      train_folds <- train_folds[1:nrow(data)]
-      train_folds <- train_folds[order(runif(length(train_folds)))]
-      train_folds[treatment == TRUE] <- NA
+      kfolds <- rep(1:kval, round(nrow(data)/kval)+1)
+      kfolds <- kfolds[order(runif(length(kfolds)))]
+      kfolds <- kfolds[1:nrow(data)]
+      kfolds <- kfolds[order(runif(length(kfolds)))]
+      kfolds[treatment == TRUE] <- NA
       
       message("..k-folds parameter detected detected")
     } else {
@@ -78,14 +90,16 @@ autoTrain <- function(args){
     }
     
   #Check if running var is specified
-    if(sum(args$run.var %in% colnames(args$data)) == 1){
+    if(sum(args$run.var %in% colnames(args$data)) == 1 & !is.null(args$rdd.thresh)){
+      #Rescale relative to threshold
       run.var <- args$data[args$run.var]
-      message("..Running var detected")
+      run.var <- run.var - args$rdd.thresh
+      message("..Running variable and threshold detected")
     } else {
-      warning("Missing running variable")
+      warning("Missing running variable and RDD threshold")
       break
     }
-    
+
   #Check if algorithm specified
       if(args$algo == 1){
         algo <- "lm"
@@ -103,27 +117,74 @@ autoTrain <- function(args){
         message(paste0("..Fixed window of ", win))
       } else  if(args$window == 2 && !is.null(args$w.size)){
         #2 = Adaptive -- create full list of options
-        win <- seq(args$w.size, max(args$data[args$run.var]),  args$w.size)
+        win.max <- ifelse(max(args$data[args$run.var]) > w.size*w.factor,
+                          w.size*w.factor,
+                          max(args$data[args$run.var]))
+                          
+        win <- seq(args$w.size, win.max,  args$w.size)
         message(paste0("..Adaptive window of ", paste(win, collapse = ", ")))
       } else {
         warning("window and w.size need to be specified")
         break
       }
   
+  #Check if window size is workable
+    for(i in win){
+      for(j in unique(kfolds)){
+        if(nrow(data[treatment == F,])==0){
+          warning("Sample size too small: Training window n = 0")
+          break
+        } else if(nrow(data[treatment == F & kfolds == j,])==0){
+          warning("Sample size too small: Training window for k-folds: n = 0")
+          break
+        } 
+      }
+    }
+
+  
 
 #PART 2: Search for best parameters
   message("Part 1 - Reviewing inputs:")
   
-  for()
-  rf.opt <- tuneRF(form.obj, data, mtryStart = 1,
-         stepFactor = 2, improve = 0.05,
-         plot = FALSE, trace = FALSE)
-  try.opt <- rf.opt[rf.opt[,2] %in% min(rf.opt[,2]),1]
-  out <- randomForest(form.obj, data, 
-                      mtry = try.opt,
-                      mtree = 500)
+  #Set up folds
+  train <- data[treatment == F,]
+  test <- data[treatment == T,]
   
+  #Set up parking lot
+    #yhat results from kcv
+    win.kcv <- data.frame()
   
+    for(j in win){
+      
+      for(k in unique(kfolds[!is.na(kfolds)])){
+        
+        #Extract window with folds
+        subtrain <- train[abs(run.var) <= j & kfolds != k,]
+        subtrain <- subtrain[complete.cases(subtrain),]
+        
+        subtest <- test[abs(run.var) <= j & kfolds == k,]
+        subtest <- subtest[complete.cases(subtest),]
+        
+        #Tune OOB
+        # rf.opt <- tuneRF(y = subtrain[[yvar.name]],
+        #                  x = subtrain[, xvar.name], mtryStart = 1,
+        #                  stepFactor = 2, improve = 0.05,
+        #                  plot = FALSE, trace = FALSE)
+        # try.opt <- rf.opt[rf.opt[,2] %in% min(rf.opt[,2]),1]
+        # 
+        #Train
+        out <- randomForest(form.obj, subtrain, 
+                            mtree = 500)
+        win.kcv <- rbind(win.kcv,
+                         data.frame(k = k,
+                                    win = j,
+                                    y = subtest[[yvar.name]],
+                                    yhat = predict(out, subtest)))
+        message(paste0("Window ", j,": kCV iter ", k))
+        
+      }
+     }
+
 }
 
 
---
+++ script/prototype_v2.R
@@ -0,0 +1,270 @@
+
+#Prototype code
+
+#Create example set
+n <- 10000
+df <- data.frame(id = 1:n,
+                 time = round((1:n)/100),
+                 y = as.numeric(1:n + rnorm(n,30,300)),
+                 x1 = rnorm(n, 100,100),
+                 x2 = rnorm(n, 200,100),
+                 x3 = runif(n)*(1:n),
+                 x4 = rnorm(n, 100,100),
+                 x5 = rnorm(n, 200,100),
+                 x6 = runif(n)*(1:n))
+df <- df[complete.cases(df),]
+tt <- runif(nrow(df)) >= 0.6
+
+
+args <- list(formula = as.formula("y~x1+x2+x3"),
+             data = df,
+             treatment = tt,
+             window = 2,
+             w.size = 10,
+             w.factor = 5,
+             algo = 2,
+             run.var = "time",
+             kval = 10,
+             rdd.thresh = 50,
+             save = FALSE)
+
+autoTrain <- function(args){
+  
+#PART 1: Triage components
+  message("Part 1 - Reviewing inputs:")
+  
+  #Check if formula exists
+    if(class(args$formula) == "formula"){
+      form.obj <- args$formula
+      yvar.name <- as.character(form.obj)[2]
+      xvar.name <- unlist(strsplit(as.character(form.obj)[3], split = "\\+"))
+      xvar.name <- trimws(xvar.name)
+      
+      message("..Formula detected")
+    } else {
+      warning("Missing formula argument.")
+      break
+    }
+    
+  #Check if data exists
+    if(class(args$data) == "data.frame"){
+      data <- args$data
+      message("..Data detected")
+    } else {
+      warning("Missing data frame")
+      break
+    }
+  
+  #Check if there are NAs
+    for(i in c(yvar.name,xvar.name)){
+      if(sum(is.na(data[,i])) > 0){
+        warning(paste0("Missing values detected in variable ", i))
+        break
+      }
+    }
+
+
+  #Check if Treatment boolean exists
+    if(class(args$treatment) == "logical"){
+      treatment <- args$treatment
+      message("..Treatment boolean detected")
+    } else {
+      warning("Missing treatment boolean")
+      break
+    }
+  
+  #Check if running var is specified
+    if(sum(args$run.var %in% colnames(args$data)) == 1 & !is.null(args$rdd.thresh)){
+      #Rescale relative to threshold
+      run.var <- args$data[args$run.var]
+      run.var <- run.var - args$rdd.thresh
+      message("..Running variable and threshold detected")
+    } else {
+      warning("Missing running variable and RDD threshold")
+      break
+    }
+
+  #Check if algorithm specified
+      if(args$algo == 1){
+        algo <- "lm"
+        message("..Use OLS")
+      } else if(args$algo == 2 || is.null(args$algo)){
+        algo <- "rf"
+        require(ranger)
+        message("..Assuming and loading RF")
+      }
+    
+    #Set window size
+      if(args$window == 1 && !is.null(args$w.size)){
+        #1 = Fixed
+        win <- args$w.size
+        message(paste0("..Fixed window of ", win))
+      } else  if(args$window == 2 && !is.null(args$w.size)){
+        #2 = Adaptive -- create full list of options
+        win.max <- ifelse(max(args$data[args$run.var]) > args$w.size * args$w.factor,
+                          args$w.size * args$w.factor,
+                          max(args$data[args$run.var]))
+                          
+        win <- seq(args$w.size, win.max,  args$w.size)
+        message(paste0("..Adaptive window of ", paste(win, collapse = ", ")))
+      } else {
+        warning("window and w.size need to be specified")
+        break
+      }
+  
+  #Check if window size is workable
+    for(i in win){
+      for(j in unique(kfolds)){
+        if(nrow(data[treatment == F,])==0){
+          warning("Sample size too small: Training window n = 0")
+          break
+        } else if(nrow(data[treatment == F & kfolds == j,])==0){
+          warning("Sample size too small: Training window for k-folds: n = 0")
+          break
+        } 
+      }
+    }
+  
+  #Check if kfolds var is specified
+  if(class(args$kval) == "numeric"){
+    kval <- args$kval
+    
+    message("..k-folds parameter detected detected")
+  } else {
+    warning("Missing k-folds parameter")
+    break
+  }
+  
+
+  
+
+#PART 2: Search for best parameters
+  message("Part 1 - Reviewing inputs:")
+  
+  #Set up folds
+  train <- data[treatment == F,]
+  test <- data[treatment == T,]
+  
+  #Predict Treatment
+    win.out <- data.frame()
+    diagnostics <- data.frame()
+    mods <- list()
+  
+    for(j in win){
+
+        #Extract window with folds
+        subtrain <- train[abs(run.var) <= j ,]
+        subtrain <- subtrain[complete.cases(subtrain),]
+        
+        subtest <- test[abs(run.var) <= j ,]
+        subtest <- subtest[complete.cases(subtest),]
+        
+        kvec <- rep(1:kval, nrow(subtrain)/kval * 1.5)
+        kvec <- sample(kvec, nrow(subtrain))
+        
+        #Train/Test
+        if(algo == "rf"){
+          #RF Option
+          
+          temp <- data.frame()
+          
+          for(k in unique(kvec)){
+            ktrain <- subtrain[kvec != k,]
+            ktrain <- ktrain[complete.cases(ktrain),]
+            
+            ktest <- subtest[kvec == k,]
+            ktest <- ktest[complete.cases(ktest),]
+            
+            out.k <- ranger(form.obj, data = ktrain, 
+                          num.trees = 500)
+            temp <- rbind(temp, 
+                          data.frame(y = ktest[[yvar.name]],
+                                     yhat = predict(out.k, ktest)$predictions))
+            message(paste0("..Fold = ",k))
+          }
+          
+          #Calculate diagnostics
+            r2 <- (cor(temp, use = "pairwise.complete.obs")[1,2])^2
+            rmse <- sqrt(mean((temp$y - temp$yhat)^2))
+            n <- nrow(temp)
+            diagnostics <- rbind(diagnostics,
+                                 data.frame(w.size = j,
+                                            n = n,
+                                            r2 = r2,
+                                            rmse = rmse))
+            
+          #Predict Control/Treatment
+            out <- ranger(form.obj, subtrain, 
+                          num.trees = 500)
+            mods[[paste0("ranger_",j)]] <- out
+            
+            win.out <- rbind(win.out,
+                             data.frame(win = j,
+                                        ytreat = subtest[[yvar.name]],
+                                        ycontrol = predict(out, subtest)$predictions))
+        } else {
+          
+          #LM Option
+          temp <- data.frame()
+          
+          for(k in unique(kvec)){
+            #split out each fold
+            ktrain <- subtrain[kvec != k,]
+            ktrain <- ktrain[complete.cases(ktrain),]
+            
+            ktest <- subtest[kvec == k,]
+            ktest <- ktest[complete.cases(ktest),]
+            
+            #Estimate
+            out.k <- lm(form.obj, data = ktrain)
+            temp <- rbind(temp, 
+                          data.frame(y = ktest[[yvar.name]],
+                                     yhat = predict(out.k, ktest)))
+            message(paste0("..Fold = ",k))
+          }
+          
+          #Calculate diagnostics
+          r2 <- (cor(temp, use = "pairwise.complete.obs")[1,2])^2
+          rmse <- sqrt(mean((temp$y - temp$yhat)^2))
+          n <- nrow(temp)
+          diagnostics <- rbind(diagnostics,
+                               data.frame(w.size = j,
+                                          n = n,
+                                          r2 = r2,
+                                          rmse = rmse))
+          
+          #Predict
+          out <- lm(form.obj, data = subtrain)
+          mods[[paste0("lm",j)]] <- out
+          
+          win.out <- rbind(win.out,
+                           data.frame(win = j,
+                                      ytreat = subtest[[yvar.name]],
+                                      ycontrol = predict(out, subtest)))
+        }
+
+        message(paste0("Window ", j))
+        
+      }
+
+  #Calculate ATE
+    win.out$te <- win.out$ytreat/win.out$ycontrol-1
+    ate <- aggregate(te ~ win,
+                     data = win.out,
+                     FUN = mean)
+    
+    
+  #Output
+    result <- list(formula = args$formula, 
+                   algorithm = class(mods[[1]]),
+                   models = mods,
+                   diagnostics = diagnostics,
+                   preds = win.out,
+                   ate = ate,
+                   rdd.thresh = args$rdd.thresh,
+                   run.var = args$run.var)
+    return(result)
+}
+
+
+out <- autoTrain(args)
